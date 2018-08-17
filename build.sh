5fc09b64186f13d0aeed8153e5e373f30ae27016
Nkumar <na388291@L-156161608.local>
2018-08-17 12:36:01 +0530

    update

--- build.sh
+++ build.sh
@@ -3,27 +3,11 @@ set -eo pipefail
 ENV=$1
 AWS_ACCOUNT_ID=$(eval "echo \$${ENV}_AWS_ACCOUNT_ID")
 AWS_REGION=$(eval "echo \$${ENV}_AWS_REGION")
+AWS_REPOSITORY=$(eval "echo \$${ENV}") 
 
 # Builds Docker image of the app.
-TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/submission-processor:$CIRCLE_SHA1
-docker build -t $TAG .
-
-# Copies "node_modules" from the created image, if necessary for caching.
-docker create --name app $TAG
-
-if [ -d node_modules ]
-then
-  mv package-lock.json old-package-lock.json
-  docker cp app:/opt/app/package-lock.json package-lock.json
-  set +eo pipefail
-  UPDATE_CACHE=$(cmp package-lock.json old-package-lock.json)
-  set -eo pipefail
-else
-  # If "node_modules" does not exist, then cache must be created.
-  UPDATE_CACHE=1
-fi
-
-if [ "$UPDATE_CACHE" == 1 ]
-then
-  docker cp app:/opt/app/node_modules .
-fi
+TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$AWS_REPOSITORY:$CIRCLE_BUILD_NUM
+sed -i='' "s|submissions-api:latest|$TAG|" docker/docker-compose.yml
+echo "" > docker/api.env
+docker-compose -f docker/docker-compose.yml build submissions-processor
+docker images
