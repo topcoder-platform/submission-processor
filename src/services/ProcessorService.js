b3adf7353e21c01dfcf208ad4b034c08b91e5b25
Sharathkumar Anbu <sharathkumaranbu@gmail.com>
2018-09-19 21:18:10 +0530

    Scan file using events

--- src/services/ProcessorService.js
+++ src/services/ProcessorService.js
@@ -6,7 +6,6 @@ const bluebird = require('bluebird')
 const config = require('config')
 const Joi = require('joi')
 const axios = require('axios')
-const FormData = require('form-data')
 const uuid = require('uuid/v4')
 const logger = require('../common/logger')
 const helper = require('../common/helper')
@@ -19,20 +18,19 @@ const AV_SCAN = 'AV Scan'
 const REVIEW_SCORECARDID = '30001850' // CWD-- TODO: make config or dynamicaly driven
 
 /**
- * Process message.
+ * Process Submission creation event
  * @param {Object} message the message
  */
-function * processMessage (message) {
+function * processCreate (message) {
   if (message.payload.resource !== 'submission') {
-    logger.info(`ignoring messages of for resource type: ${message.resource}`)
+    logger.info(`ignoring messages of resource type: ${message.resource}`)
     return false
   }
 
   // check whether the submission file is at DMZ area
-  let dmzS3Obj
   const fileName = message.payload.id + '.' + message.payload.fileType
   try {
-    dmzS3Obj = yield s3p.getObjectAsync({ Bucket: config.get('aws.DMZ_BUCKET'), Key: fileName })
+    yield s3p.getObjectAsync({ Bucket: config.get('aws.DMZ_BUCKET'), Key: fileName })
     // the file is already in DMZ area
     logger.info(`The file ${fileName} is already in DMZ area.`)
   } catch (e) {
@@ -44,42 +42,21 @@ function * processMessage (message) {
     logger.info(`The file ${fileName} is not in DMZ area, copying it to DMZ area.`)
     const downloadedFile = yield helper.downloadFile(message.payload.url)
     yield s3p.uploadAsync({ Bucket: config.get('aws.DMZ_BUCKET'), Key: fileName, Body: downloadedFile })
-    dmzS3Obj = yield s3p.getObjectAsync({ Bucket: config.get('aws.DMZ_BUCKET'), Key: fileName })
   }
 
-  // scan the file in DMZ
-  logger.info(`Scanning the file ${fileName}.`)
-  const form = new FormData()
-  form.append('file', dmzS3Obj.Body, { filename: fileName })
-  const scanResult = yield axios.post(config.ANTIVIRUS_API_URL, form, { maxContentLength: config.MAXFILESIZE, headers: form.getHeaders() })
-  let destinationBucket = config.get('aws.CLEAN_BUCKET')
-  if (!scanResult.data.infected) {
-    logger.info(`The file ${fileName} is clean. Moving file to clean submission area.`)
-  } else {
-    logger.info(`The file ${fileName} is infected. Moving file to quarantine area.`)
-    destinationBucket = config.get('aws.QUARANTINE_BUCKET')
-  }
-
-  yield helper.moveFile(config.get('aws.DMZ_BUCKET'), fileName, destinationBucket, fileName)
-  const movedS3Obj = `https://s3.amazonaws.com/${destinationBucket}/${fileName}`
-  logger.debug(`moved file: ${JSON.stringify(movedS3Obj)}`)
-  logger.info('Update Submission final location using Submission API')
-  yield helper.reqToSubmissionAPI('PATCH', `${config.SUBMISSION_API_URL}/submissions/${message.payload.id}`,
-    { url: movedS3Obj })
-
-  logger.info('Create review using Submission API')
-  yield helper.reqToSubmissionAPI('POST', `${config.SUBMISSION_API_URL}/reviews`, {
-    score: scanResult.data.infected ? 0 : 100,
-    reviewerId: uuid(), //  CWD-- TODO: should fix this to a specific Id
+  // Send request to Scan the file
+  logger.info(`Sending request to scan the file ${fileName}.`)
+  const reqBody = {
     submissionId: message.payload.id,
-    scoreCardId: REVIEW_SCORECARDID,
-    typeId: yield helper.getreviewTypeId(AV_SCAN)
-  })
+    url: message.payload.url,
+    fileName: fileName
+  }
+  yield axios.post(config.ANTIVIRUS_API_URL, reqBody, { maxContentLength: config.MAXFILESIZE })
 
   return true
 }
 
-processMessage.schema = {
+processCreate.schema = {
   message: Joi.object().keys({
     topic: Joi.string().required(),
     originator: Joi.string().required(),
@@ -95,9 +72,57 @@ processMessage.schema = {
   }).required()
 }
 
+/**
+ * Process Scan completion event
+ * @param {Object} message the message
+ */
+function * processScan (message) {
+  let destinationBucket = config.get('aws.CLEAN_BUCKET')
+  const fileName = message.payload.fileName
+  if (!message.payload.isInfected) {
+    logger.info(`The file ${fileName} is clean. Moving file to clean submission area.`)
+  } else {
+    logger.info(`The file ${fileName} is infected. Moving file to quarantine area.`)
+    destinationBucket = config.get('aws.QUARANTINE_BUCKET')
+  }
+
+  yield helper.moveFile(config.get('aws.DMZ_BUCKET'), fileName, destinationBucket, fileName)
+  const movedS3Obj = `https://s3.amazonaws.com/${destinationBucket}/${fileName}`
+  logger.debug(`moved file: ${JSON.stringify(movedS3Obj)}`)
+  logger.info('Update Submission final location using Submission API')
+  yield helper.reqToSubmissionAPI('PATCH', `${config.SUBMISSION_API_URL}/submissions/${message.payload.submissionId}`,
+    { url: movedS3Obj })
+
+  logger.info('Create review using Submission API')
+  yield helper.reqToSubmissionAPI('POST', `${config.SUBMISSION_API_URL}/reviews`, {
+    score: message.payload.isInfected ? 0 : 100,
+    reviewerId: uuid(), //  CWD-- TODO: should fix this to a specific Id
+    submissionId: message.payload.submissionId,
+    scoreCardId: REVIEW_SCORECARDID,
+    typeId: yield helper.getreviewTypeId(AV_SCAN)
+  })
+}
+
+processScan.schema = {
+  message: Joi.object().keys({
+    topic: Joi.string().required(),
+    originator: Joi.string().required(),
+    timestamp: Joi.date().required(),
+    'mime-type': Joi.string().required(),
+    payload: Joi.object().keys({
+      submissionId: Joi.string().required(),
+      url: Joi.string().required(),
+      fileName: Joi.string().required(),
+      status: Joi.string().required(),
+      isInfected: Joi.boolean().required()
+    }).unknown(true).required()
+  }).required()
+}
+
 // Exports
 module.exports = {
-  processMessage
+  processCreate,
+  processScan
 }
 
 logger.buildService(module.exports)
